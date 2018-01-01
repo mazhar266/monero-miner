779351a3dd4991222fc2116e163f7640ba258320
Syed Mazhar Ahmed <mazhar@interconnectionbd.com>
2018-01-01 11:05:33 +0600

    updated the API keys

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('WwUvv2hVIPkWFjx8ZkTcka0rU189YogK'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
